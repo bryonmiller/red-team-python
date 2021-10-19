677ab3eac4e0955ba5d859c07eec21224ba7b251
bryonmiller <bryon.c.miller@gmail.com>
2021-10-19 16:33:23 -0500

    Working using the Requests and pprint libraries. Intermediate version of project reads from ips.txt and prett prints data from API for each IP.

--- IPGeoAutomated.py
+++ IPGeoAutomated.py
@@ -1,3 +1,17 @@
+import requests # Requests allows for HTTP requests to be used in the program
+from pprint import pprint # Pretty print formatting
 
+# Creating variable to hold the IP and asking user for input
+# ip = input("What IP would you like to check? ")
 
-print ("test")
+# Get IP(s) from file
+f = open("ips.txt","r")
+ip = f.readlines()
+
+# For loop to request data for each IP
+for i in ip:
+    data = requests.get('https://api.ipgeolocation.io/ipgeo?apiKey=f80001219f4e463494bcfd2fb08b92a3&ip='+i).json()
+    pprint(data)
+
+# For use with a single IP entered by the user
+# print (requests.get('https://api.ipgeolocation.io/ipgeo?apiKey=f80001219f4e463494bcfd2fb08b92a3&ip='+ip).json())

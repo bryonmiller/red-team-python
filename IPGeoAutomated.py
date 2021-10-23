608ca0870cdce3e7159e9701bee9b39315e490e3
bryonmiller <bryon.c.miller@gmail.com>
2021-10-23 00:17:22 -0500

    Making changes to add functionality for CIDR notated IP networks

--- IPGeoAutomated.py
+++ IPGeoAutomated.py
@@ -1,4 +1,5 @@
 import requests # Requests allows for HTTP requests to be used in the program
+import ipaddress # For IP address manipulation
 from pprint import pprint # Pretty print formatting
 
 # Creating variable to hold the IP and asking user for input
@@ -9,9 +10,20 @@ f = open("ips.txt","r")
 ip = f.readlines()
 
 # For loop to request data for each IP
-for i in ip:
-    data = requests.get('https://api.ipgeolocation.io/ipgeo?apiKey=API_TOKEN&ip='+i).json()
-    pprint(data)
+def scopeQuery():
+    for i in ip:
+        if '/' in i:
+            addrs = ipaddress.ip_network(i.replace("\n",'')) # Check for CIDR and remove lines
+            for addr in addrs:
+                data = requests.get('https://api.ipgeolocation.io/ipgeo?apiKey=a8563127af3f479eb97d9b6ef34810ae&ip='+str(addr)).json()
+                print(data)
+        else:
+            #data = requests.get('https://api.ipgeolocation.io/ipgeo?apiKey=a8563127af3f479eb97d9b6ef34810ae&ip='+i).json()
+            #fwrite = open('scopedata.txt', 'w')
+            #fwrite.write = (data)
+            print ("skipped")
+
+scopeQuery()
 
 # For use with a single IP entered by the user
-# print (requests.get('https://api.ipgeolocation.io/ipgeo?apiKey=f80001219f4e463494bcfd2fb08b92a3&ip='+ip).json())
+# print (requests.get('https://api.ipgeolocation.io/ipgeo?apiKey=API_TOKEN&ip='+ip).json())
